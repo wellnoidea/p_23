288e9e79a0f5ad0382565cbf591824290d5b5a7f
wellnoidea <stewern@gmail.com>
2015-05-21 17:51:41 +0200

    added notes

--- script.js
+++ script.js
@@ -2,21 +2,27 @@
 // READ: http://www.sitepoint.com/basic-animation-with-canvas-and-javascript/
 // read: http://mdn.beonex.com/en/Canvas_tutorial/Optimizing_canvas.html
 
+'use strict';
+
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillStyle = '#00FF00';	// set a fill color
+context.fillStyle = '#00FF00';    // set a fill color
 
-var theLoop = function (timestamp) {	
-	timestamp = timestamp || 0;
-	// clear the canvas, otherwise moving objects will create ,lines‘
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.fillRect(
-		  0 + timestamp / 100	// upper left corner x
-		, 0 + timestamp / 50	// upper left corner Y
-		,50						// width
-		,50);					// height
-	window.requestAnimationFrame(theLoop);
+var theLoop = function (timestamp) {
+	
+	var _lol = 44;
+	
+    		timestamp = timestamp || 0;
+    // clear the canvas, otherwise moving objects will create ,lines‘
+						    context.clearRect(0, 0, canvas.width, canvas.height);
+context.fillRect(
+					timestamp / 100,    // upper left corner x
+		timestamp / 50,     // upper left corner Y
+		50,                        // width
+        50
+    );                    // height
+    									window.requestAnimationFrame(theLoop);
 };
 
 // call the requestAnimationFrame for the first time
